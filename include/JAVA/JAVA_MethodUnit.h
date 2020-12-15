7e1e1b6d82475d546b12a5afdff4a3499908a4a9
Arystan <a_r_s_t_a_n@mail.ru>
2020-12-15 18:08:13 +0600

    init

---
+++ include/JAVA/JAVA_MethodUnit.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include "include/MethodUnit.h"
+
+class JAVA_MethodUnit : public MethodUnit
+{
+public:
+	// См. MethodUnit.h
+	explicit JAVA_MethodUnit(const std::string& name, const std::string& returnType, const Configuration::Flags& flags);
+	void add(const Configuration::Ptr& unit, const Configuration::Flags& flags) override;
+	std::string compile(const Configuration::UI& level) const override;
+};
+
+inline JAVA_MethodUnit::JAVA_MethodUnit(const std::string& name, const std::string& returnType, const Configuration::Flags& flags) :
+	MethodUnit(name, returnType, flags)
+{
+}
+
+inline void JAVA_MethodUnit::add(const Configuration::Ptr& unit, const Configuration::Flags& /*flags*/)
+{
+	if (unit == nullptr)
+		THROW(__FUNCTION__);
+
+	// Добавляем в тело метода юнит
+	_body.push_back(unit);
+}
+
+inline std::string JAVA_MethodUnit::compile(const Configuration::UI& level) const
+{
+	std::string result;
+
+	// По аналогии с CSHARP_MethodUnit проверяем наличие установленых битов в 1 
+	// Для работы с многопоточностью
+	if (_flags & Configuration::Modifier::SYNCHRONIZED)
+		result += "synchronized ";
+
+	// Метод статичный
+	if (_flags & Configuration::Modifier::STATIC)
+		result += "static ";
+
+	// Метод абстрактный ( почти как чистая функция в плюсах )
+	if (_flags & Configuration::Modifier::ABSTRACT)
+		result += "abstract ";
+
+	// Метод помечен как final
+	else if (_flags & Configuration::Modifier::FINAL)
+		result += "final ";
+
+	// Добавляем возвращаемый тип и имя метода
+	result += _returnType + ' ' + _name + "()" + " {\n";
+
+	// Проходим по всему телу и генереруем метод
+	for (const auto& it : _body)
+		result += it->compile(level + 1);
+
+	// По аналогии с CPP_MethodUnit
+	result += generateShift(level) + "}\n";
+
+	return result;
+}
---
+++ include/JAVA/JAVA_PrintUnit.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "include/PrintUnit.h"
+
+class JAVA_PrintUnit : public PrintUnit
+{
+public:
+	// См. PrintUnit
+	explicit JAVA_PrintUnit(const std::string& text);
+	void add(const Configuration::Ptr& unit, const Configuration::Flags& flags) override {}
+	std::string compile(const Configuration::UI& level) const override;
+};
+
+inline JAVA_PrintUnit::JAVA_PrintUnit(const std::string& text) :
+	PrintUnit(text)
+{
+}
+
+inline std::string JAVA_PrintUnit::compile(const Configuration::UI& level) const
+{
+	// Вывод в консоль на языке JAVA
+	return generateShift(level) + "System.out.println( \"" + _text + "\" );\n";
+}
